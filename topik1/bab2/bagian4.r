b94a9bfd684865c0c832000bddab58f3f9bbd21a
sianturiwilly <prajudiwilliam10@gmail.com>
2022-07-22 17:16:52 +0700

    Commit ketiga

--- topik1/bab2/bagian4.r
+++ topik1/bab2/bagian4.r
@@ -1,9 +1,18 @@
-# angka <- c(20:30)
-# print(angka)
-# print(angka[3])
-# print(angka[[5]])
-# print(angka[4:6])
+angka <- c(20:30)
 
+print(angka)
+
+# Tampilkan isi variable angka pada posisi ke 3
+print(angka[3])
+
+# Tampilkan isi variable angka pada posisi ke 5
+print(angka[[5]])
+
+# Tampilkan isi variable angka pada posisi ke 4 s/d 6
+print(angka[4:6])
+
+# Buat vector teks dengan nama kode_prodi yang diisi sesuai petunjuk soal"
 kode_prodi <- c("DKV","ILKOM","ICT")
-print(kode_prodi)
+
+# Tampilkan isi indeks ketiga dari kode_prodi
 print(kode_prodi[3])
