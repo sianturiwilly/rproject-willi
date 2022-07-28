3a819e550f91288020ba549c2ef46a3f9667d74c
sianturiwilly <prajudiwilliam10@gmail.com>
2022-07-28 14:01:06 +0700

    fourth commit

---
+++ modul4/bagian1.R
@@ -0,0 +1,4 @@
+# install.packages("ggplot2")
+library(ggplot2)
+#Ketik function ggplot() di bawah ini
+ggplot()
---
+++ modul4/bagian4.R
@@ -0,0 +1,3 @@
+plot.jakarta <- ggplot()
+plot.jakarta <- plot.jakarta + labs(title="Luas Wilayah vs Kepadatan Penduduk DKI Jakarta - Periode 2013")
+plot.jakarta
