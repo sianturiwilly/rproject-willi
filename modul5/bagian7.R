1f0e1c8a492ca1cea3d59d8596cc753dc995fc25
sianturiwilly <prajudiwilliam10@gmail.com>
2022-08-03 23:32:07 +0700

    Commit ke-16

---
+++ modul5/bagian7.R
@@ -0,0 +1,15 @@
+#Bagian Data Preparation
+pelanggan <- read.csv("https://storage.googleapis.com/dqlab-dataset/customer_segments.txt", sep="\t")
+pelanggan_matrix <- data.matrix(pelanggan[c("Jenis.Kelamin", "Profesi", "Tipe.Residen")])
+pelanggan <- data.frame(pelanggan, pelanggan_matrix)
+Profesi <- unique(pelanggan[c("Profesi","Profesi.1")])
+Jenis.Kelamin <- unique(pelanggan[c("Jenis.Kelamin","Jenis.Kelamin.1")])
+Tipe.Profesi <- unique(pelanggan[c("Tipe.Residen","Tipe.Residen.1")])
+pelanggan$NilaiBelanjaSetahun <- pelanggan$NilaiBelanjaSetahun/1000000
+field_yang_digunakan = c("Jenis.Kelamin.1", "Umur", "Profesi.1", "Tipe.Residen.1","NilaiBelanjaSetahun")
+#Bagian K-Means
+set.seed(100)
+#fungsi kmeans untuk membentuk 5 cluster dengan 25 skenario random dan simpan ke dalam variable segmentasi
+segmentasi <- kmeans(x=pelanggan[c("Umur","Profesi.1")], centers=3, nstart=25)
+#tampilkan hasil k-means
+segmentasi
---
+++ modul5/bagian8.R
@@ -0,0 +1,29 @@
+#Bagian Data Preparation
+
+pelanggan <- read.csv("https://storage.googleapis.com/dqlab-dataset/customer_segments.txt", sep="\t")
+
+pelanggan_matrix <- data.matrix(pelanggan[c("Jenis.Kelamin", "Profesi", "Tipe.Residen")])
+
+pelanggan <- data.frame(pelanggan, pelanggan_matrix)
+
+Profesi <- unique(pelanggan[c("Profesi","Profesi.1")])
+
+Jenis.Kelamin <- unique(pelanggan[c("Jenis.Kelamin","Jenis.Kelamin.1")])
+
+Tipe.Profesi <- unique(pelanggan[c("Tipe.Residen","Tipe.Residen.1")])
+
+pelanggan$NilaiBelanjaSetahun <- pelanggan$NilaiBelanjaSetahun/1000000
+
+field_yang_digunakan = c("Jenis.Kelamin.1", "Umur", "Profesi.1", "Tipe.Residen.1","NilaiBelanjaSetahun")
+
+#Bagian K-Means
+
+set.seed(100)
+
+segmentasi <- kmeans(x=pelanggan[field_yang_digunakan], centers=5, nstart=25)
+
+segmentasi$cluster
+
+#Penggabungan hasil cluster
+
+pelanggan$cluster <- segmentasi$cluster
