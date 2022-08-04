0ee2b4e75b4481cf0169dffad1f21daf97a77bd1
sianturiwilly <prajudiwilliam10@gmail.com>
2022-08-04 10:25:49 +0700

    Commit ke-17

--- modul5/bagian12.R
+++ modul5/bagian12.R
@@ -1,33 +1,16 @@
-# Analisa Hasil Clustering Vector
+# Materi: Analisa Hasil Sum of Squares
 
 #Bagian Data Preparation
-
 pelanggan <- read.csv("https://storage.googleapis.com/dqlab-dataset/customer_segments.txt", sep="\t")
-
 pelanggan_matrix <- data.matrix(pelanggan[c("Jenis.Kelamin", "Profesi", "Tipe.Residen")])
-
 pelanggan <- data.frame(pelanggan, pelanggan_matrix)
-
 Profesi <- unique(pelanggan[c("Profesi","Profesi.1")])
-
 Jenis.Kelamin <- unique(pelanggan[c("Jenis.Kelamin","Jenis.Kelamin.1")])
-
 Tipe.Profesi <- unique(pelanggan[c("Tipe.Residen","Tipe.Residen.1")])
-
 pelanggan$NilaiBelanjaSetahun <- pelanggan$NilaiBelanjaSetahun/1000000
-
 field_yang_digunakan = c("Jenis.Kelamin.1", "Umur", "Profesi.1", "Tipe.Residen.1","NilaiBelanjaSetahun")
-
-#Bagian K-Means
-
+#Membandingkan dengan 2 cluster kmeans, masing-masing 2 dan 5
 set.seed(100)
-
-segmentasi <- kmeans(x=pelanggan[field_yang_digunakan], centers=5, nstart=25)
-
-segmentasi$cluster
-
-#Penggabungan hasil cluster
-
-pelanggan$cluster <- segmentasi$cluster
-
-str(pelanggan)
+kmeans(x=pelanggan[field_yang_digunakan], centers=2, nstart=25)
+set.seed(100)
+kmeans(x=pelanggan[field_yang_digunakan], centers=5, nstart=25)
