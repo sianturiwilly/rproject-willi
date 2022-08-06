e9ff1862058a00945f2d86508cac50d01992e388
sianturiwilly <prajudiwilliam10@gmail.com>
2022-08-06 22:22:42 +0700

    Commit ke-25

--- modul8/bagian5.R
+++ modul8/bagian5.R
@@ -1,3 +1,9 @@
 # Modul 8: Project Analisa Klasifikasi Pinjaman untuk Sektor UMKM
 # Bab 1: Pendahuluan
 # Materi: Konversi Data
+
+# data_reduce = data[-c(1,2)]
+# colnames(data_reduce)
+
+# cek.1 <- data_reduce[, 8:11]
+# head(cek.1, 9)
